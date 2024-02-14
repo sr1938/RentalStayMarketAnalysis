0b04d14f809aed3cd840788141bc56745d59c867
mayurbonde5479 <148853498+mayurbonde5479@users.noreply.github.com>
2024-02-14 23:24:33 +0530

    Variables.tf

--- terraform/Variables.tf
+++ terraform/Variables.tf
@@ -5,3 +5,48 @@ locals {
 variable "project" {
     type=string
 }
+
+variable "engine_name" {
+  description = "Enter the DB engine"
+  type        = string
+  default     = "mysql"
+}
+
+
+variable "db_name" {
+  description = "Enter the name of the database to be created inside DB Instance"
+  type        = string
+  default     = "group4"
+}
+variable "user_name" {
+  description = "Enter the username for DB"
+  type        = string
+  default     = "group4_dbda"
+}
+variable "pass" {
+  description = "Enter the password for DB"
+  type        = string
+  default     = "group4123"
+}
+variable "multi_az_deployment" {
+  description = "Enable or disable multi-az deployment"
+  type        = bool
+  default     = false
+}
+variable "public_access" {
+  description = "Whether public access needed"
+  type        = bool
+  default     = true
+}
+variable "skip_finalSnapshot" {
+  type    = bool
+  default = true
+}
+variable "delete_automated_backup" {
+  type    = bool
+  default = true
+}
+variable "instance_class" {
+  type    = string
+  default = "db.t2.micro"
+}
