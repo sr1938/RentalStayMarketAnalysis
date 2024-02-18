e70952a0c81af39c210359710fddf170d256e21f
shubhamrajput768 <shubhamr1938@gmail.com>
2024-02-18 18:45:36 +0530

    updated

--- deploy/extraction.tf
+++ deploy/extraction.tf
@@ -8,41 +8,46 @@ provider "aws" {
 #     byte_length = 8
 # }
 
+#------------------- STEP FUNCTION TO TRIGGER GLUE JOB ---------------#
+#  Define an SNS topic :
 
+resource "aws_sns_topic" "glue_job_notification" {
+  name = "glue-job-notification-topic"
+}
 
-# ####--------------------------------------- S3 bucket --------------------------------------------####
-# resource "aws_s3_bucket" "raw_data_zone" {
-#     bucket = "rawdata-${random_id.random_id_generator.hex}"
+####--------------------------------------- S3 bucket --------------------------------------------####
+resource "aws_s3_bucket" "scripts" {
+    bucket = "glue-scripts"
     
-#     tags = {
-#         project_type = "demo"
-#     } 
-# }
-# resource "aws_s3_bucket_ownership_controls" "raw_data_zone" {
-#   bucket = aws_s3_bucket.raw_data_zone.id
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
-# resource "aws_s3_bucket_public_access_block" "raw_data_zone" {
-#   bucket = aws_s3_bucket.raw_data_zone.id
+    tags = {
+        project_type = "demo"
+    } 
+}
+resource "aws_s3_bucket_ownership_controls" "scripts" {
+  bucket = aws_s3_bucket.scripts.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "scripts" {
+  bucket = aws_s3_bucket.scripts.id
 
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 
-# resource "aws_s3_bucket_acl" "raw_data_zone" {
-#   depends_on = [
-#     aws_s3_bucket_ownership_controls.raw_data_zone,
-#     aws_s3_bucket_public_access_block.raw_data_zone,
-#   ]
+resource "aws_s3_bucket_acl" "scripts" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.scripts,
+    aws_s3_bucket_public_access_block.scripts,
+  ]
 
-#   bucket = aws_s3_bucket.raw_data_zone.id
-#   acl    = "public-read"
-# }
+  bucket = aws_s3_bucket.scripts.id
+  acl    = "public-read"
+}
 
 ####--------------------------------------- EC2 Instance --------------------------------------------####
 
@@ -80,48 +85,48 @@ resource "aws_security_group" "main" {
 
 # ####--------------------------------------- EMR --------------------------------------------####
 
-resource "aws_emr_cluster" "emr_cluster" {
-  name          = "emr-ingest-rds"
-  release_label = "emr-6.15.0"
-  applications  = ["Spark"]
+# resource "aws_emr_cluster" "emr_cluster" {
+#   name          = "emr-ingest-rds"
+#   release_label = "emr-6.15.0"
+#   applications  = ["Spark"]
 
-  termination_protection            = false
-  keep_job_flow_alive_when_no_steps = true
+#   termination_protection            = false
+#   keep_job_flow_alive_when_no_steps = true
 
-  ec2_attributes {
-    emr_managed_master_security_group = aws_security_group.main.id
-    emr_managed_slave_security_group  = aws_security_group.main.id
-    instance_profile                  = "EMR_EC2_DefaultRole"
-  }
+#   ec2_attributes {
+#     emr_managed_master_security_group = aws_security_group.main.id
+#     emr_managed_slave_security_group  = aws_security_group.main.id
+#     instance_profile                  = "EMR_EC2_DefaultRole"
+#   }
 
-  master_instance_group {
-    instance_type = "m5.xlarge"
-  }
+#   master_instance_group {
+#     instance_type = "m5.xlarge"
+#   }
 
-  ebs_root_volume_size = 30
+#   ebs_root_volume_size = 30
 
-  tags = {
-    Name = "EMR-RDS-S3"
-    role = "rolename"
-    env  = "env"
-  }
+#   tags = {
+#     Name = "EMR-RDS-S3"
+#     role = "rolename"
+#     env  = "env"
+#   }
 
-  service_role = "EMR_DefaultRole"
-}
+#   service_role = "EMR_DefaultRole"
+# }
 
 # ####--------------------------------------- RDS --------------------------------------------####
 
 resource "aws_db_instance" "rds_ingest_instance" {
-  engine                   = var.engine_name
-  db_name                  = var.db_name
-  username                 = var.user_name
-  password                 = var.pass
-  skip_final_snapshot      = var.skip_finalSnapshot
-  delete_automated_backups = var.delete_automated_backup
-  multi_az                 = var.multi_az_deployment
-  publicly_accessible      = var.public_access
-  instance_class           = var.instance_class
+  engine                   = "mysql"
+  db_name                  = "group4"
+  username                 = "admin"
+  password                 = "123456789"
+  skip_final_snapshot      = true
+  delete_automated_backups = true
+  multi_az                 = false
+  publicly_accessible      = true
+  instance_class           = "db.t3.micro"
   allocated_storage        = 20
-  availability_zone        = var.availability_zone
+  availability_zone        = "us-east-1c"
   
 }
--- deploy/transformation.tf
+++ deploy/transformation.tf
@@ -1,21 +1,22 @@
-# s3 object to upload the file
-resource "aws_s3_object" "data_cleaning_glue_script" {
-    bucket = "group4-raw-data-zone"
-    
-    key = "/TestDeploy.py"
-    source = "./TestDeploy.py"
-    tags = {
-        project_type = "demo"
-    }
+# uploading objects to s3
+resource "aws_s3_object" "upload-glue-script-1" {
+  bucket = aws_s3_bucket.scripts.id
+  key    = "first_job.py"
+  source = "./first_job.py"
+}
+
+resource "aws_s3_object" "upload-glue-script-2" {
+  bucket = aws_s3_bucket.scripts.id
+  key    = "second_job.py"
+  source = "./second_job.py"
 }
 
 
-
-resource "aws_glue_job" "glue_deploy" {
+resource "aws_glue_job" "glue_job_1" {
   glue_version = "4.0" 
   max_retries = 0 
-  name = "Cleaning_Deploy" 
-  description = "importing, cleaning and preprocessing" 
+  name = "Ingestion_job" 
+  description = "Ingesting data from s3" 
   role_arn = "arn:aws:iam::199657276973:role/LabRole"
   
   number_of_workers = 2 
@@ -27,7 +28,7 @@ resource "aws_glue_job" "glue_deploy" {
   }
   command {
     name="gluel" 
-    script_location = "s3://group4-raw-data-zone/TestDeploy.py" 
+    script_location = "s3://${aws_s3_bucket.scripts.id}/first_job.py" 
   }
   default_arguments = {
     "--output-dir"              = "s3://group4-enrich-data-zone/"
@@ -41,3 +42,87 @@ resource "aws_glue_job" "glue_deploy" {
     "--conf"                    = "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions  --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog  --conf spark.sql.catalog.glue_catalog.warehouse=s3://tnt-erp-sql/ --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog  --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO"
   }
 }
+
+
+resource "aws_glue_job" "glue_job_2" {
+  glue_version = "4.0" 
+  max_retries = 0 
+  name = "Cleaning_job" 
+  description = "Cleaning and preprocessing" 
+  role_arn = "arn:aws:iam::199657276973:role/LabRole"
+  
+  number_of_workers = 2 
+  worker_type = "G.1X" 
+  timeout = "60" 
+  
+  tags = {
+    project = "final_project" 
+  }
+  command {
+    name="glue2" 
+    script_location = "s3://${aws_s3_bucket.scripts.id}/second_job.py" 
+  }
+  default_arguments = {
+    "--output-dir"              = "s3://group4-enrich-data-zone/"
+    "--class"                   = "GlueApp"
+    "--enable-job-insights"     = "true"
+    "--enable-auto-scaling"     = "false"
+    "--enable-glue-datacatalog" = "true"
+    "--job-language"            = "python"
+    "--job-bookmark-option"     = "job-bookmark-disable"
+    "--datalake-formats"        = "iceberg"
+    "--conf"                    = "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions  --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog  --conf spark.sql.catalog.glue_catalog.warehouse=s3://tnt-erp-sql/ --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog  --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO"
+  }
+}
+
+
+#---------- STEP FUNCTION TO TRIGGER GLUE JOB AND NOTIFY---------------#
+resource "aws_sfn_state_machine" "glue_job_trigger" {
+  name     = "glue-job-trigger"
+  role_arn = "arn:aws:iam::199657276973:role/LabRole"
+
+  definition = <<EOF
+{
+  "Comment": "Firstly, Combining Data and Secondly Cleaning and modifying the data",
+  "StartAt": "GlueJob1",
+  "States": {
+    "GlueJob1": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::glue:startJobRun.sync",
+      "Parameters": {
+        "JobName": "${aws_glue_job.glue_job_1.name}"
+      },
+      "Next": "SNSPublish1"
+    },
+    "SNSPublish1": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::sns:publish",
+      "Parameters": {
+        "TopicArn": "${aws_sns_topic.glue_job_notification.arn}",
+        "Message": "Greetings Group 4,\n\nYour Glue Job 1 is completed successfully."
+      },
+      "Next": "GlueJob2"
+    },
+    "GlueJob2": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::glue:startJobRun.sync",
+      "Parameters": {
+        "JobName": "${aws_glue_job.glue_job_2.name}"
+      },
+      "Next": "SNSPublish2"
+    },
+    "SNSPublish2": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::sns:publish",
+      "Parameters": {
+        "TopicArn": "${aws_sns_topic.glue_job_notification.arn}",
+        "Message": "Greetings Group 4,\n\nYour Glue Job 2 is completed successfully\n\nThe Data is cleaned and loaded to enriched zone Successfully."
+      },
+      "End": true
+    }
+  }
+}
+EOF
+}
+
+
