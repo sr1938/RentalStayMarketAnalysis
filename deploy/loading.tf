8fb312cafa86cf0eaa2f064fe268e7bfc9c0a027
shubhamrajput768 <shubhamr1938@gmail.com>
2024-02-22 12:32:48 +0530

    change of backend

--- deploy/loading.tf
+++ deploy/loading.tf
@@ -42,6 +42,7 @@ resource "aws_athena_workgroup" "rental_market_analysis_workgroup" {
 
 configuration {
     result_configuration {
+      
         output_location = "s3://group4-enrich-data-zone/queryresults"
     }
 
